60e17a5d6944e64d60f2c5c8cd53968a7ae0530d
elodloic <95284344+elodloic@users.noreply.github.com>
2024-05-22 18:02:13 +0200

    Update scripts.js
    Created pokemonList array and added first objects.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
-alert('Hello world');
-let favoriteFood = 'Lasagna';
-document.write(favoriteFood);
+var pokemonList = [
+    {name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
+    {name: 'Squirtle', height: 0.5, types: ['water']},
+    {name: 'Butterfree', height: 1.1, types: ['bug', 'flying']},
+];
