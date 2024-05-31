c5305311b11c770c53b6e7a25ac1d479b880a5a8
elodloic <95284344+elodloic@users.noreply.github.com>
2024-05-31 17:32:12 +0200

    Updated index.html and scripts.js
    App now displays the array objects and adds a comment based on the "height" property.

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,11 @@ var pokemonList = [
     {name: 'Squirtle', height: 0.5, types: ['water']},
     {name: 'Butterfree', height: 1.1, types: ['bug', 'flying']},
 ];
+
+for (let i = 0; i < pokemonList.length; i++) 
+    if (pokemonList[i].height >= 1) {
+        document.write('<p>' + pokemonList[i].name + ' (height: '+ pokemonList[i].height + ') - Wow, that’s big!</p>');
+    }
+    else {
+        document.write('<p>' + pokemonList[i].name + ' (height: '+ pokemonList[i].height + ')</p>');
+    }
