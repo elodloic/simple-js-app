ddbd48a2404ce20a9e1c93fe1c5d78b823d91578
elodloic <95284344+elodloic@users.noreply.github.com>
2024-06-05 10:27:32 +0200

    Update scripts.js
    Improved variable declaration and string concatenation.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-var pokemonList = [
+const pokemonList = [
     {name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
     {name: 'Squirtle', height: 0.5, types: ['water']},
     {name: 'Butterfree', height: 1.1, types: ['bug', 'flying']},
@@ -6,8 +6,8 @@ var pokemonList = [
 
 for (let i = 0; i < pokemonList.length; i++) 
     if (pokemonList[i].height >= 1) {
-        document.write('<p>' + pokemonList[i].name + ' (height: '+ pokemonList[i].height + ') - Wow, that’s big!</p>');
+        document.write(`<p>${pokemonList[i].name} (height: ${pokemonList[i].height}) - Wow, that’s big!</p>`);
     }
     else {
-        document.write('<p>' + pokemonList[i].name + ' (height: '+ pokemonList[i].height + ')</p>');
+        document.write(`<p>${pokemonList[i].name} (height: ${pokemonList[i].height})</p>`);
     }
