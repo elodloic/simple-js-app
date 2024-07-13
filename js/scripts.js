aa32690b44f891a034a937196d474a5a51df03ed
elodloic <95284344+elodloic@users.noreply.github.com>
2024-07-13 17:17:13 +0200

    Modals implemented
    Updated JS, HTML and CSS files to accommodate modals after clicking on one of the pokemon buttons.

--- js/scripts.js
+++ js/scripts.js
@@ -44,7 +44,7 @@ let pokemonRepository = (function () {
         let listItem = document.createElement('li');
         let button = document.createElement('button');
         button.innerText = pokemon.name;
-        button.classList.add('pokemon-card');
+        button.classList.add('pokemon-button');
         listItem.appendChild(button);
         container.appendChild(listItem);
         button.addEventListener('click', function (event) {
@@ -61,12 +61,66 @@ let pokemonRepository = (function () {
     };
 })();
 
+let modalContainer = document.querySelector('#modal-container');
+
 function showDetails(pokemon) {
-  pokemonRepository.loadDetails(pokemon).then(function () {
-    console.log(pokemon);
+  pokemonRepository.loadDetails(pokemon).then(function() {
+      showModal(
+      pokemon.name, 
+      'Height: ' + pokemon.height,
+      pokemon.imageUrl
+     );
   });
 }
 
+function showModal(title, text, img) {
+  modalContainer.innerHTML = '';
+
+  let modal = document.createElement('div');
+  modal.classList.add('modal');
+
+  let closeButtonElement = document.createElement('button');
+  closeButtonElement.classList.add('modal-close');
+  closeButtonElement.innerText = "Close";
+  closeButtonElement.addEventListener('click', hideModal);
+
+  let pokemonName = document.createElement('h1');
+  pokemonName.innerText = title;
+
+  let pokemonHeight = document.createElement('p');
+  pokemonHeight.innerText = text;
+
+  let pokemonImage = document.createElement('img');
+  pokemonImage.setAttribute('src', img);
+  pokemonImage.setAttribute("width", "100%");
+  pokemonImage.setAttribute("height", "100%");
+
+  modal.appendChild(closeButtonElement);
+  modal.appendChild(pokemonName);
+  modal.appendChild(pokemonHeight);
+  modal.appendChild(pokemonImage);
+  modalContainer.appendChild(modal);
+
+  modalContainer.classList.add('is-visible');
+
+  modalContainer.addEventListener('click', (e) => {
+      let target = e.target;
+      if (target === modalContainer) {
+          hideModal();
+      }
+  });
+}
+
+
+function hideModal() {
+  modalContainer.classList.remove('is-visible');
+}
+
+window.addEventListener('keydown', (e) => {
+  if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+      hideModal();
+  }
+});
 
 pokemonRepository.loadList().then(function() {
   // Data is loaded!
